aeb07c985ce40ac771baf16abf9251a570f74008
trimzej <trimz@hispeed.ch>
2024-02-28 20:27:32 +0100

    created name.java and all attributes

---
+++ src/main/java/ch/zhaw/babynames2/model/Name.java
@@ -0,0 +1,31 @@
+package ch.zhaw.babynames2.model;
+
+public class Name {
+    private String name;
+    private String geschlecht;
+    private int anzahl;
+
+    public Name(String name, String geschlecht, int anzahl) {
+        this.name = name;
+        this.geschlecht = geschlecht;
+        this.anzahl = anzahl;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getGeschlecht() {
+        return geschlecht;
+    }
+
+    public int getAnzahl() {
+        return anzahl;
+    }
+
+    @Override
+    public String toString() {
+        return "Name [name=" + name + ", geschlecht=" + geschlecht + ", anzahl=" + anzahl + "]";
+    }
+
+}
