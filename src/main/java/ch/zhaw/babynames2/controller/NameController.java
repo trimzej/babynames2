b7328c92af87e35c2a44924afd02dd255c913bc8
trimzej <trimz@hispeed.ch>
2024-02-28 21:00:45 +0100

    created "controller" folder, added NameController.java and created Endpoints

---
+++ src/main/java/ch/zhaw/babynames2/controller/NameController.java
@@ -0,0 +1,42 @@
+package ch.zhaw.babynames2.controller;
+
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.opencsv.CSVReader;
+
+import ch.zhaw.babynames2.model.Name;
+
+@RestController
+public class NameController {
+    private ArrayList<Name> listOfNames;
+
+    @GetMapping("/names")
+    public ArrayList<Name> getNames() {
+        return listOfNames;
+    }
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void runAfterStartup() throws Exception {
+        listOfNames = new ArrayList<>();
+        Path path = Paths.get(ClassLoader.getSystemResource("data/babynames.csv").toURI());
+        System.out.println("Read from: " + path);
+        try (Reader reader = Files.newBufferedReader(path)) {
+            try (CSVReader csvReader = new CSVReader(reader)) {
+                String[] line;
+                while ((line = csvReader.readNext()) != null) {
+                    listOfNames.add(new Name(line[0], line[1], Integer.parseInt(line[2])));
+                }
+                System.out.println("Read " + listOfNames.size() + " names");
+            }
+        }
+    }
+}
