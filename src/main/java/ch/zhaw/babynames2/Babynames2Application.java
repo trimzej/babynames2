aef4b441f7370a563ac5c1eb67f6d6cf58b10286
trimzej <trimz@hispeed.ch>
2024-02-28 13:28:57 +0100

    first commit

---
+++ src/main/java/ch/zhaw/babynames2/Babynames2Application.java
@@ -0,0 +1,13 @@
+package ch.zhaw.babynames2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Babynames2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Babynames2Application.class, args);
+	}
+
+}
---
+++ src/test/java/ch/zhaw/babynames2/Babynames2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.zhaw.babynames2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Babynames2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
