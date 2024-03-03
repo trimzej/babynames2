259e5b08dff944b201ab678fa8896e4fcfbb010d
trimzej <trimz@hispeed.ch>
2024-03-03 19:20:50 +0100

    lombok name.java change

--- src/main/java/ch/zhaw/babynames2/model/Name.java
+++ src/main/java/ch/zhaw/babynames2/model/Name.java
@@ -4,31 +4,11 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.ToString;
 
+@AllArgsConstructor
+@Getter
+@ToString
 public class Name {
     private String name;
     private String geschlecht;
     private int anzahl;
-
-    public Name(String name, String geschlecht, int anzahl) {
-        this.name = name;
-        this.geschlecht = geschlecht;
-        this.anzahl = anzahl;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getGeschlecht() {
-        return geschlecht;
-    }
-
-    public int getAnzahl() {
-        return anzahl;
-    }
-
-    @Override
-    public String toString() {
-        return "Name [name=" + name + ", geschlecht=" + geschlecht + ", anzahl=" + anzahl + "]";
-    }
 }
