692e75a4b2b7813a096d96bf1870e3407b9f3f3f
trimzej <trimz@hispeed.ch>
2024-03-03 15:46:58 +0100

    Name.java lombok change

--- src/main/java/ch/zhaw/babynames2/model/Name.java
+++ src/main/java/ch/zhaw/babynames2/model/Name.java
@@ -1,5 +1,9 @@
 package ch.zhaw.babynames2.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
 public class Name {
     private String name;
     private String geschlecht;
@@ -27,5 +31,4 @@ public class Name {
     public String toString() {
         return "Name [name=" + name + ", geschlecht=" + geschlecht + ", anzahl=" + anzahl + "]";
     }
-
 }
